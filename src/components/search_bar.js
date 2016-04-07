5b853a83e4643a5aa85f102e8471d744cea61473
Spacemixup <xchrislee@gmail.com>
2016-04-07 15:22:04 -0700

    SearchBar.js made as well as branch files.

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+//functional component
+const SearchBar = () => {
+	return <input type="text"/>;
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyB_-yxPxNAssDHut65MBgyVifPmt1PcVAg';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce html.
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+//takes 2 inputs - component and insert it into the dom element
+ReactDOM.render(<App />, document.querySelector('.container'));
